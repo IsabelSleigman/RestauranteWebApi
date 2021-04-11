7073b4ff3a129ac6e844f1388dfe522ab7928930
Isabel Sleigman <Sleimusic@gmail.com>
2021-04-11 16:03:30 -0300

    COmana OK

--- RestauranteFront/restaurante-app/src/app/home/comanda/comanda.service.ts
+++ RestauranteFront/restaurante-app/src/app/home/comanda/comanda.service.ts
@@ -1,3 +1,4 @@
+import { ModelCompleta } from './models/modelCompleta';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
@@ -5,7 +6,7 @@ import { BehaviorSubject, Observable } from 'rxjs';
 import { take } from 'rxjs/operators';
 import { environment } from 'src/environments/environment';
 import { BuscarModel } from './models/buscar-model';
-import { ModelPaga } from './models/modelPaga';
+
 
 @Injectable({ providedIn: 'root' })
 
@@ -14,20 +15,14 @@ export class ComandaService {
     baseUrl = `${environment.apiUrl}/comanda/`
 
     public comandaId: number;
-    private _comanda = new BehaviorSubject<ModelPaga>(null);
-    public readonly comanda$: Observable<ModelPaga> = this._comanda.asObservable();
+    private _comanda = new BehaviorSubject<ModelCompleta>(null);
+    public readonly comanda$: Observable<ModelCompleta> = this._comanda.asObservable();
 
     constructor(private http: HttpClient,
         private router: Router,
         private route: ActivatedRoute) {
     }
    
-    obterComanda(comandaId: number): Observable<BuscarModel> {
-        return this.http
-            .get<BuscarModel>(this.baseUrl + comandaId)
-            .pipe(
-                take(1));
-    }
-
+   
 
 }
