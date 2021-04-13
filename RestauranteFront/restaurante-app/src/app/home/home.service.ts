b1bcc8bc8f1cc88efc54b2f8debdbd7fcd7b2ee0
Isabel Sleigman <Sleimusic@gmail.com>
2021-04-13 11:40:38 -0300

    Refatorando

--- RestauranteFront/restaurante-app/src/app/home/home.service.ts
+++ RestauranteFront/restaurante-app/src/app/home/home.service.ts
@@ -1,5 +1,4 @@
 import { ModelCompleta } from './comanda/models/modelCompleta';
-
 import { AberturaModel } from './../inicial/models/abertura-model';
 import { Injectable } from "@angular/core";
 import { catchError, take } from 'rxjs/operators';
@@ -7,13 +6,12 @@ import { BehaviorSubject, Observable } from "rxjs";
 import { environment } from "src/environments/environment";
 import { HttpClient, HttpErrorResponse } from "@angular/common/http";
 import { ActivatedRoute, Params, Router } from '@angular/router';
+import { NotificationService } from '../shared/snackbar/notification.service';
 const baseUrl = `${environment.apiUrl}/comanda`
 
 @Injectable()
 
 export class HomeService {
-
-
 
     public comandaId: number;
     private _comanda = new BehaviorSubject<ModelCompleta>(null);
@@ -21,7 +19,8 @@ export class HomeService {
 
     constructor(private http: HttpClient,
         private router: Router,
-        private route: ActivatedRoute) {
+        private route: ActivatedRoute,
+        private snackbar: NotificationService) {
     }
 
     iniciar(iniciar: AberturaModel) {
@@ -29,11 +28,16 @@ export class HomeService {
         return this.http
             .post<number>(`${baseUrl}`, iniciar)
             .pipe(
-                take(1)).subscribe(id => {
+                take(1),
+                catchError((error: HttpErrorResponse) => {
+                    this.snackbar.errorMessage(error);
+                    throw error;
+                 }) 
+                 ).subscribe(id => {
                     this.comandaId = id;
                     const c = this._comanda.getValue();
                     this._comanda.next(c);
-                    this.router.navigate(["home", this.comandaId], { relativeTo: this.route })
+                    this.router.navigate(["home", this.comandaId], { relativeTo: this.route }) //retirar a navegação daqui
                 });
 
     }
@@ -42,20 +46,33 @@ export class HomeService {
         return this.http
             .get<ModelCompleta>(`${baseUrl}/${this.comandaId}/completa`)
             .pipe(
-                take(1),catchError((error: HttpErrorResponse) => {
+                take(1),
+                catchError((error: HttpErrorResponse) => {
+                    this.snackbar.errorMessage(error);
                     throw error;
-                })).subscribe(res => this._comanda.next(res));
+                 })
+                ).subscribe(res => this._comanda.next(res));
     }
 
     fecharComanda() {
         this.http
             .post(`${baseUrl}/${this.comandaId}/fechar`,this.comandaId)
             .pipe( 
-                take(1), catchError((error: HttpErrorResponse) => {
-                    throw error;
-                   
-                })).subscribe(() => {});
+                take(1), 
+                catchError((error: HttpErrorResponse) => {
+                    this.snackbar.errorMessage(error);
+                    throw error;       
+                })
+                ).subscribe(() => {});
                 
     }
 
+    obterComandaId(){
+
+        let comandaId : number = 0;
+        this._comanda.subscribe(c => c.comandaId = c.comandaId);
+        return comandaId;
+
+    }
+    
 }
